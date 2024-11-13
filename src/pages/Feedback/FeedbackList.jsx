0c2ae24899ff8c0d3be255bbced85958cfbcdcbe
hyhy-j <lhjlhj10088@gmail.com>
2024-11-13 23:58:36 +0900

    [Feat] 피드백 목록 페이지 - 채택된 피드백 있을 시 UI 구현 완료

--- src/pages/Feedback/FeedbackList.jsx
+++ src/pages/Feedback/FeedbackList.jsx
@@ -6,9 +6,11 @@ import Back from "../../assets/images/Back.svg";
 import WriteFeedbackIcon from "../../assets/images/WriteFeedbackIcon.svg";
 import AIFeedbackIcon from "../../assets/images/AIFeedbackIcon.svg";
 import NoFeedbackFrog from "../../assets/images/NoFeedbackFrog.svg";
+import SeeDetailIcon from "../../assets/images/SeeDetailIcon.svg";
 
 const FeedbackListContainer = styled.div`
   margin-top: 30px;
+  padding-bottom: 50px;
 `;
 
 const BackImg = styled.img`
@@ -97,13 +99,73 @@ const NoSelectedFeedback = styled.div`
   }
 `;
 
+const SelectedFeedbackWrapper = styled.div`
+  margin-top: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 15px;
+  overflow-y: auto;
+`;
+
+const SelectedFeedback = styled.div`
+  padding: 20px 30px;
+  background-color: rgba(51, 51, 51, 1);
+  border-radius: 8px;
+  width: 50vw;
+  height: 100px;
+
+  p {
+    font-size: 11px;
+    font-weight: 200;
+  }
+`;
+
+const InfonDetailBtnWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    color: white;
+    font-size: 11px;
+  }
+`;
+
+const NickNamenDateWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 5px;
+  margin-bottom: 12px;
+
+  h8 {
+    font-size: 12px;
+  }
+  h9 {
+    color: rgba(153, 153, 153, 1);
+    font-size: 12px;
+  }
+`;
+
 function FeedbackList() {
   const navigate = useNavigate();
 
   // 임의의 변수들
   const username = "user1";
   const projectuser = "user12";
-  const feedbackList = [];
+  const feedbackList = [
+    { nickname: "닉네임", date: "0000.00.00.", content: "이슈 내용 미리보기" },
+    { nickname: "닉네임", date: "0000.00.00.", content: "이슈 내용 미리보기" },
+    { nickname: "닉네임", date: "0000.00.00.", content: "이슈 내용 미리보기" },
+    { nickname: "닉네임", date: "0000.00.00.", content: "이슈 내용 미리보기" },
+    { nickname: "닉네임", date: "0000.00.00.", content: "이슈 내용 미리보기" },
+  ];
 
   return (
     <div>
@@ -132,7 +194,23 @@ function FeedbackList() {
         <SelectedFeedbackContainer>
           <p>채택된 피드백 목록</p>
           {feedbackList.length > 0 ? (
-            <></>
+            <SelectedFeedbackWrapper>
+              {feedbackList.map((item, index) => (
+                <SelectedFeedback key={index}>
+                  <InfonDetailBtnWrapper>
+                    <NickNamenDateWrapper>
+                      <h8>{item.nickname}</h8>
+                      <h9>{item.date}</h9>
+                    </NickNamenDateWrapper>
+                    <button>
+                      상세보기
+                      <img src={SeeDetailIcon} alt="SeeDetailIcon" />
+                    </button>
+                  </InfonDetailBtnWrapper>
+                  <p>{item.content}</p>
+                </SelectedFeedback>
+              ))}
+            </SelectedFeedbackWrapper>
           ) : (
             <NoSelectedFeedback>
               <img src={NoFeedbackFrog} alt="frog" />
