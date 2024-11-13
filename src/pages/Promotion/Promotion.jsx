7c4ee9b195ebe3dbd5ae874151c31bd5f38fdf4b
hyhy-j <lhjlhj10088@gmail.com>
2024-11-13 19:45:09 +0900

    [Feat] 광고 선택시 광고 버튼 색상 변경

--- src/pages/Promotion/Promotion.jsx
+++ src/pages/Promotion/Promotion.jsx
@@ -14,30 +14,49 @@ const AdvertisementWrapper = styled.div`
   align-items: center;
   justify-content: center;
   gap: 40px;
+`;
 
-  button {
-    background-color: rgba(63, 65, 66, 1);
-    border: 1px solid rgba(118, 118, 118, 1);
-    border-radius: 8px;
-    width: 190px;
-    height: 200px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    color: white;
+const AdvertisementBtn = styled.button`
+  background-color: ${(props) =>
+    props.isSelected ? "rgba(0, 193, 58, 0.1)" : "rgba(63, 65, 66, 1)"};
+  border: ${(props) =>
+    props.isSelected
+      ? "1px solid rgba(0, 193, 58, 1)"
+      : "1px solid rgba(118, 118, 118, 1)"};
+  border-radius: 8px;
+  width: 190px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
 
-    img {
-      margin-bottom: 20px;
-    }
+  img {
+    margin-bottom: 20px;
+  }
 
-    h4 {
-      margin-bottom: 5px;
-    }
+  h4 {
+    margin-bottom: 5px;
+  }
+
+  &:hover {
+    background-color: rgba(0, 193, 58, 0.1);
+    border: 1px solid rgba(0, 193, 58, 1);
   }
 `;
 
 function Promotion() {
+  const [selectBanner, setSelectBanner] = useState(false);
+  const [selectModal, setSelectModal] = useState(false);
+
+  const handleSelectBannerBtn = () => {
+    setSelectBanner(!selectBanner);
+  };
+
+  const handleSelectModalBtn = () => {
+    setSelectModal(!selectModal);
+  };
   return (
     <div>
       <Header />
@@ -52,16 +71,22 @@ function Promotion() {
           </p>
         </PhraseContainer>
         <AdvertisementWrapper>
-          <button>
+          <AdvertisementBtn
+            onClick={handleSelectBannerBtn}
+            isSelected={selectBanner}
+          >
             <img src={BannerAdvertisement} alt="BannerAdvertisement" />
             <h4>배너 광고</h4>
             <p>월 2만원</p>
-          </button>
-          <button>
+          </AdvertisementBtn>
+          <AdvertisementBtn
+            onClick={handleSelectModalBtn}
+            isSelected={selectModal}
+          >
             <img src={ModalAdvertisement} alt="ModalAdvertisement" />
             <h4>모달창 광고</h4>
             <p>월 3만원</p>
-          </button>
+          </AdvertisementBtn>
         </AdvertisementWrapper>
       </PromotionContainer>
     </div>
