1ee8070fd4f98d28babe811b68168e85dd0a08c3
gn00py48 <gn00py48@gmail.com>
2024-11-15 02:27:34 +0900

    [feat] #5 project detail page layout and api

--- src/api/projectComment.js
+++ src/api/projectComment.js
@@ -3,10 +3,10 @@ import { instance } from "./instance";
 export const getProjectComments = async (project_id) => {
     try {
         const response = await instance.get(`/api/project_detail/${project_id}/comment`);
-        console.log("댓글 데이터 가져오기 성공:", response.data); // 성공 시 데이터 확인용 로그
+        console.log("댓글 데이터 가져오기 성공:", response.data); // 성공용 메세지~!~!
         return response.data;
     } catch (error) {
-        console.error("댓글 데이터 가져오기 에러:", error); // 에러 발생 시 로그
+        console.error("댓글 데이터 가져오기 에러:", error); // 에러 로그~!~!
         throw error;
     }
 };
