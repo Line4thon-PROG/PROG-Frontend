faf65d34c5d5cd4a168e9daeee2233b995e71af6
hyhy-j <lhjlhj10088@gmail.com>
2024-11-15 13:27:35 +0900

    [Fix] Search페이지 프로젝트 wrapper 크기 오류 수정

--- src/pages/Project/Search.jsx
+++ src/pages/Project/Search.jsx
@@ -124,7 +124,7 @@ const SelectedTag = styled.span`
 const ProjectWrapper = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: flex-start;
   width: 53.8vw;
   flex-wrap: wrap;
   position: relative;
