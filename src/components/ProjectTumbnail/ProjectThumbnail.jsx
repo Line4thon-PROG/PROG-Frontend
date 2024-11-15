12cb419e770b955bcbe724582d00909341141037
hyhy-j <lhjlhj10088@gmail.com>
2024-11-15 21:01:06 +0900

    [Add] Search 페이지 디자인 수정

--- src/components/ProjectTumbnail/ProjectThumbnail.jsx
+++ src/components/ProjectTumbnail/ProjectThumbnail.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const RecommendContainer = styled.div`
+  width: 257px;
   cursor: pointer;
   img {
     width: 257px;
@@ -17,6 +18,20 @@ const GenrenSkill = styled.p`
   margin-top: 4px;
   color: rgba(153, 153, 153, 1);
   font-size: 10px;
+  white-space: nowrap;
+`;
+const StringWrapper = styled.div`
+  width: 257px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  overflow-x: auto;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 function ProjectThumbnail({
@@ -30,11 +45,15 @@ function ProjectThumbnail({
   return (
     <RecommendContainer onClick={() => navigate(`/DetailPage/${project_id}`)}>
       <img src={imagesrc} alt="ProjectThumbnailImage" />
-      <Title>{name}</Title>
-      <GenrenSkill>
-        {genrelist && genrelist.length && genrelist.join(" | ")} |{" "}
-        {skilllist && skilllist.length && skilllist.join(" | ")}
-      </GenrenSkill>
+      <StringWrapper>
+        <Title>{name}</Title>
+      </StringWrapper>
+      <StringWrapper>
+        <GenrenSkill>
+          {genrelist && genrelist.length && genrelist.join(" | ")} |{" "}
+          {skilllist && skilllist.length && skilllist.join(" | ")}
+        </GenrenSkill>
+      </StringWrapper>
     </RecommendContainer>
   );
 }
